d47d91db8f47c0549f13fd68993b7a0ef6a74571
Marcin Antonik <marcin.antonik91@gmail.com>
2023-04-26 19:11:03 +0200

    Add display product names in capital letters

--- Shopping_list.py
+++ Shopping_list.py
@@ -1,7 +1,7 @@
 shopping_list= {}
 count = 0
 shopping_list = {'piekarnia' : ['chleb', 'bułki', 'pączek'], 'warzywniak' : ['marchew', 'seler', 'rukola']}
-print("Lista zakupów")
-for element in shopping_list:
-    print(f"Idę do {element.capitalize()} i kupuję tam {[element.capitalize() for element in shopping_list[element]]}")
-    count += len(shopping_list[element])
+print ("Lista zakupów")
+for product in shopping_list:
+    print(f"Idę do {product.capitalize()} i kupuję tam {[element.capitalize() for element in shopping_list[product]]}")
+    count += len(shopping_list[product])
