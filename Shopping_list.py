aec35602e95737e421f9974b112d8767dd4f37fd
Marcin Antonik <marcin.antonik91@gmail.com>
2023-04-26 20:39:59 +0200

    Add numbers of shops

--- Shopping_list.py
+++ Shopping_list.py
@@ -5,4 +5,4 @@ print ("Lista zakupów z podziałem na sklepy")
 for product in shopping_list:
     print(f"Idę do {product.capitalize()} i kupuję tam {[element.capitalize() for element in shopping_list[product]]}")
     count += len(shopping_list[product])
-print (f"W sumie kupuję {count} produktów.")
+print (f"W sumie kupuję {count} produktów w {len(shopping_list.items())} sklepach.")
