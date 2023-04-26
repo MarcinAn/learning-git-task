8abb0fb63ce40b8f452822b21a51b768ec67f774
Marcin Antonik <marcin.antonik91@gmail.com>
2023-04-26 20:33:08 +0200

    Add corrected header

--- Shopping_list.py
+++ Shopping_list.py
@@ -1,7 +1,7 @@
 shopping_list= {}
 count = 0
 shopping_list = {'piekarnia' : ['chleb', 'bułki', 'pączek'], 'warzywniak' : ['marchew', 'seler', 'rukola']}
-print ("Lista zakupów")
+print ("Lista zakupów z podziałem na sklepy")
 for product in shopping_list:
     print(f"Idę do {product.capitalize()} i kupuję tam {[element.capitalize() for element in shopping_list[product]]}")
     count += len(shopping_list[product])
