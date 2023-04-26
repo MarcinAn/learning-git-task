b0eba78befabcddbf92855b54d58882210513a7e
Marcin Antonik <marcin.antonik91@gmail.com>
2023-04-26 19:05:00 +0200

    Add for loop and displaying product names

--- Shopping_list.py
+++ Shopping_list.py
@@ -1,3 +1,7 @@
 shopping_list= {}
 count = 0
 shopping_list = {'piekarnia' : ['chleb', 'bułki', 'pączek'], 'warzywniak' : ['marchew', 'seler', 'rukola']}
+print("Lista zakupów")
+for element in shopping_list:
+    print(f"Idę do {element.capitalize()} i kupuję tam {[element.capitalize() for element in shopping_list[element]]}")
+    count += len(shopping_list[element])
