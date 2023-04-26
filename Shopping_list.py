0a012e837daf2a1e16f84411bba9b581476c4030
Marcin Antonik <marcin.antonik91@gmail.com>
2023-04-26 19:13:14 +0200

    Add product quantities and displaying a summary

--- Shopping_list.py
+++ Shopping_list.py
@@ -5,3 +5,4 @@ print ("Lista zakupów")
 for product in shopping_list:
     print(f"Idę do {product.capitalize()} i kupuję tam {[element.capitalize() for element in shopping_list[product]]}")
     count += len(shopping_list[product])
+print (f"W sumie kupuję {count} produktów.")
