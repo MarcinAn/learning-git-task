afafa0199cd17ebc9c962c829350c485ebc31356
Marcin <80240893+MarcinAn@users.noreply.github.com>
2023-04-27 08:41:21 +0200

    fix spacing

--- Shopping_list.py
+++ Shopping_list.py
@@ -1,8 +1,8 @@
 shopping_list= {}
 count = 0
 shopping_list = {'piekarnia' : ['chleb', 'bułki', 'pączek'], 'warzywniak' : ['marchew', 'seler', 'rukola']}
-print ("Lista zakupów z podziałem na sklepy")
+print("Lista zakupów z podziałem na sklepy")
 for product in shopping_list:
     print(f"Idę do {product.capitalize()} i kupuję tam {[element.capitalize() for element in shopping_list[product]]}")
     count += len(shopping_list[product])
-print (f"W sumie kupuję {count} produktów w {len(shopping_list.items())} sklepach.")
+print(f"W sumie kupuję {count} produktów w {len(shopping_list.items())} sklepach.")
